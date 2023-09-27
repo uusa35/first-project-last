8cca400cf140e0b13bfa5957a2283fcce5c3ac1f
uusa35 <uusa35@gmail.com>
2023-09-27 07:04:42 +0300

    prepare structure

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -131,26 +131,31 @@ export default function ComingSoon() {
             }}
           />
         </div>
-        <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
-          <div className=' sm:mb-8 sm:flex sm:justify-center'>
+        <div
+          className='absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
+          aria-hidden='true'>
+          <div
+            className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#44e87b] to-[#039703] opacity-60 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 min-h-screen'>
+          <div className=' sm:mb-8 flex justify-center items-center'>
             <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600'></div>
-
             <Image src={Logo} className='h-16 w-16' alt='' fill={false} />
           </div>
-          <div className='text-center leading-loose'>
+          <div className='text-center'>
             <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl capitalize leading-loose'>
               {soon}
             </h1>
-            <p className='mt-6 text-6xl font-extrabold leading-loose text-gray-600 capitalize py-16'>
+            <p className='text-6xl font-extrabold leading-loose text-gray-600 capitalize py-16'>
               {ar_expo_ru}
             </p>
-            <p className='mt-6 text-2xl font-extrabold leading-loose text-gray-600'>
-              <span>المنتدى </span>
 
-              <span className='text-green-600'>العربي </span>
-              <span className='text-blue-600'> الروسي</span>
-            </p>
-            <div className='mt-10 flex items-center justify-center gap-x-6'>
+            <div className='flex items-center justify-center gap-x-6'>
               <Link
                 href='/'
                 className='hidden rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
@@ -163,17 +168,6 @@ export default function ComingSoon() {
               </Link>
             </div>
           </div>
-        </div>
-        <div
-          className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
-          aria-hidden='true'>
-          <div
-            className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#44e87b] to-[#039703] opacity-60 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
         </div>
       </div>
     </div>
