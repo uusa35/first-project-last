6f8e451515b1a04a66aead07a61f03c0bc822f06
uusa35 <uusa35@gmail.com>
2023-10-15 22:00:16 +0300

    some changes to logo and homepage

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -12,6 +12,7 @@ import MainSlider from "@/components/MainSlider";
 import Image from "next/image";
 import { getImages } from "@/utils/image";
 import { getUsers } from "@/utils/user";
+import { User } from "@/types/queries";
 
 const tiers = [
   {
@@ -201,6 +202,7 @@ export default async function Home({ params: { lang } }: Props) {
     subscriptions,
     sponsorships,
     posts,
+    sponsors,
     images,
   ] = await Promise.all([
     getDictionary(lang),
@@ -567,41 +569,18 @@ export default async function Home({ params: { lang } }: Props) {
             {trans.sponsors}
           </h2>
           <div className='mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5'>
-            <img
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              src='https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg'
-              alt='Transistor'
-              width={158}
-              height={48}
-            />
-            <img
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              src='https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg'
-              alt='Reform'
-              width={158}
-              height={48}
-            />
-            <img
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              src='https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg'
-              alt='Tuple'
-              width={158}
-              height={48}
-            />
-            <img
-              className='col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1'
-              src='https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg'
-              alt='SavvyCal'
-              width={158}
-              height={48}
-            />
-            <img
-              className='col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1'
-              src='https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg'
-              alt='Statamic'
-              width={158}
-              height={48}
-            />
+            {sponsors.data.map((s: User, i: string) => (
+              <Link href={`/user/${s.id}?slug=${s.name}`}>
+                <img
+                  key={i}
+                  className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
+                  src={s.image}
+                  alt='Transistor'
+                  width={100}
+                  height={100}
+                />
+              </Link>
+            ))}
           </div>
         </div>
       </div>
