d01e5aa471e2163ce22691f9aaf6045b09854d95
uusa35 <uusa35@gmail.com>
2023-10-16 09:19:55 +0300

    aboutus

--- app/[lang]/aboutus/page.tsx
+++ app/[lang]/aboutus/page.tsx
@@ -15,194 +15,78 @@ export default async function Aboutus({
 
   return (
     <MainContextLayout trans={trans} lang={lang} searchParams={``}>
-      <div className='relative bg-white mx-auto max-w-7xl'>
-        <div className='lg:absolute lg:inset-0 ltr:lg:left-1/2 rtl:lg:right-1/2'>
+      <main className='relative isolate mx-auto max-w-7xl'>
+        {/* Image section */}
+        <div className='mt-8 sm:mt-8 xl:mx-auto xl:max-w-7xl xl:px-8'>
           <img
-            className='h-64 w-full bg-gray-50 object-cover sm:h-80 lg:absolute lg:h-full'
-            src='https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80'
+            src='https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2894&q=80'
             alt=''
+            className='aspect-[9/4] w-full object-cover xl:rounded-3xl'
           />
         </div>
-        <div className='pb-24 pt-16 sm:pb-32 sm:pt-24 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:pt-32'>
-          <div className='px-6 lg:px-8'>
-            <div className='mx-auto max-w-xl lg:mx-0 lg:max-w-lg'>
-              <h2 className='text-3xl font-bold tracking-tight text-gray-900'>
-                {trans.aboutus}
-              </h2>
-              <p className='mt-2 text-lg leading-8 text-gray-600'>
-                Proin volutpat consequat porttitor cras nullam gravida at orci
-                molestie a eu arcu sed ut tincidunt magna.
-              </p>
-              <form action='#' method='POST' className='mt-16'>
-                <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
-                  <div>
-                    <label
-                      htmlFor='first-name'
-                      className='block text-sm font-semibold leading-6 text-gray-900'>
-                      First name
-                    </label>
-                    <div className='mt-2.5'>
-                      <input
-                        type='text'
-                        name='first-name'
-                        id='first-name'
-                        autoComplete='given-name'
-                        className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor='last-name'
-                      className='block text-sm font-semibold leading-6 text-gray-900'>
-                      Last name
-                    </label>
-                    <div className='mt-2.5'>
-                      <input
-                        type='text'
-                        name='last-name'
-                        id='last-name'
-                        autoComplete='family-name'
-                        className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                      />
-                    </div>
-                  </div>
-                  <div className='sm:col-span-2'>
-                    <label
-                      htmlFor='email'
-                      className='block text-sm font-semibold leading-6 text-gray-900'>
-                      Email
-                    </label>
-                    <div className='mt-2.5'>
-                      <input
-                        id='email'
-                        name='email'
-                        type='email'
-                        autoComplete='email'
-                        className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                      />
-                    </div>
-                  </div>
-                  <div className='sm:col-span-2'>
-                    <label
-                      htmlFor='company'
-                      className='block text-sm font-semibold leading-6 text-gray-900'>
-                      Company
-                    </label>
-                    <div className='mt-2.5'>
-                      <input
-                        type='text'
-                        name='company'
-                        id='company'
-                        autoComplete='organization'
-                        className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                      />
-                    </div>
-                  </div>
-                  <div className='sm:col-span-2'>
-                    <div className='flex justify-between text-sm leading-6'>
-                      <label
-                        htmlFor='phone'
-                        className='block font-semibold text-gray-900'>
-                        Phone
-                      </label>
-                      <p id='phone-description' className='text-gray-400'>
-                        Optional
-                      </p>
-                    </div>
-                    <div className='mt-2.5'>
-                      <input
-                        type='tel'
-                        name='phone'
-                        id='phone'
-                        autoComplete='tel'
-                        aria-describedby='phone-description'
-                        className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                      />
-                    </div>
-                  </div>
-                  <div className='sm:col-span-2'>
-                    <div className='flex justify-between text-sm leading-6'>
-                      <label
-                        htmlFor='message'
-                        className='block text-sm font-semibold leading-6 text-gray-900'>
-                        How can we help you?
-                      </label>
-                      <p id='message-description' className='text-gray-400'>
-                        Max 500 characters
-                      </p>
-                    </div>
-                    <div className='mt-2.5'>
-                      <textarea
-                        id='message'
-                        name='message'
-                        rows={4}
-                        aria-describedby='message-description'
-                        className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                        defaultValue={""}
-                      />
-                    </div>
-                  </div>
-                  <fieldset className='sm:col-span-2'>
-                    <legend className='block text-sm font-semibold leading-6 text-gray-900'>
-                      Expected budget
-                    </legend>
-                    <div className='mt-4 space-y-4 text-sm leading-6 text-gray-600'>
-                      <div className='flex gap-x-2.5'>
-                        <input
-                          id='budget-under-25k'
-                          name='budget'
-                          defaultValue='under_25k'
-                          type='radio'
-                          className='mt-1 h-4 w-4 border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-600'
-                        />
-                        <label htmlFor='budget-under-25k'>Less than $25K</label>
-                      </div>
-                      <div className='flex gap-x-2.5'>
-                        <input
-                          id='budget-25k-50k'
-                          name='budget'
-                          defaultValue='25k-50k'
-                          type='radio'
-                          className='mt-1 h-4 w-4 border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-600'
-                        />
-                        <label htmlFor='budget-25k-50k'>$25K – $50K</label>
-                      </div>
-                      <div className='flex gap-x-2.5'>
-                        <input
-                          id='budget-50k-100k'
-                          name='budget'
-                          defaultValue='50k-100k'
-                          type='radio'
-                          className='mt-1 h-4 w-4 border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-600'
-                        />
-                        <label htmlFor='budget-50k-100k'>$50K – $100K</label>
-                      </div>
-                      <div className='flex gap-x-2.5'>
-                        <input
-                          id='budget-over-100k'
-                          name='budget'
-                          defaultValue='over_100k'
-                          type='radio'
-                          className='mt-1 h-4 w-4 border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-600'
-                        />
-                        <label htmlFor='budget-over-100k'>$100K+</label>
-                      </div>
-                    </div>
-                  </fieldset>
-                </div>
-                <div className='mt-10 flex justify-end border-t border-gray-900/10 pt-8'>
-                  <button
-                    type='submit'
-                    className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-                    Send message
-                  </button>
-                </div>
-              </form>
-            </div>
+        {/* Header section */}
+        <div className='px-6 pt-12 lg:px-8'>
+          <div className='mx-auto max-w-2xl text-center '>
+            <h2 className='text-4xl font-bold tracking-tight text-black sm:text-6xl'>
+              We love creators
+            </h2>
+            <p className='mt-6 text-lg leading-8 text-gray-800'>
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
+            </p>
           </div>
         </div>
-      </div>
+
+        {/* Content section */}
+        <div className='mx-auto mt-8 max-w-7xl px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl lg:mx-0 lg:max-w-none'>
+            <div className='grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-800 lg:max-w-none lg:grid-cols-2'>
+              <div>
+                <p>
+                  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
+                  risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+                  Id dolor praesent donec est. Odio penatibus risus viverra
+                  tellus varius sit neque erat velit. Faucibus commodo massa
+                  rhoncus, volutpat. Dignissim sed eget risus enim. Mattis
+                  mauris semper sed amet vitae sed turpis id.
+                </p>
+                <p className='mt-8'>
+                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien
+                  duis odio id et. Id blandit molestie auctor fermentum
+                  dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
+                  varius vulputate et ultrices hac adipiscing egestas.
+                </p>
+              </div>
+              <div>
+                <p>
+                  Erat pellentesque dictumst ligula porttitor risus eget et
+                  eget. Ultricies tellus felis id dignissim eget. Est augue
+                  maecenas risus nulla ultrices congue nunc tortor. Enim et
+                  nesciunt doloremque nesciunt voluptate.
+                </p>
+                <p className='mt-8'>
+                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien
+                  duis odio id et. Id blandit molestie auctor fermentum
+                  dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
+                  varius vulputate et ultrices hac adipiscing egestas. Iaculis
+                  convallis ac tempor et ut. Ac lorem vel integer orci.
+                </p>
+              </div>
+            </div>
+            <dl className='mt-16 grid grid-cols-1 gap-x-8 gap-y-12 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mt-28 lg:grid-cols-4'>
+              <div
+                // key={statIdx}
+                className='flex flex-col-reverse gap-y-3 border-l border-white/20 pl-6'>
+                <dt className='text-base leading-7 text-gray-800'>test</dt>
+                <dd className='text-3xl font-semibold tracking-tight text-black'>
+                  test
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </main>
     </MainContextLayout>
   );
 }
