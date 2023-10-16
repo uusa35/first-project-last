f5a8b84ab2fab6ef30b0916adf3b68d47df07fb5
uusa35 <uusa35@gmail.com>
2023-10-16 09:05:51 +0300

    commenting slider according to build error

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -65,9 +65,9 @@ export default function NavHeader({ lang, searchParams = `` }: Props) {
           </button>
         </div>
         <div className='hidden lg:flex lg:gap-x-8'>
-          {navigation.map((item) => (
+          {navigation.map((item, i) => (
             <Link
-              key={item.name}
+              key={i}
               href={item.href}
               className='text-sm font-semibold leading-6 text-gray-900'>
               {item.name}
@@ -134,9 +134,9 @@ export default function NavHeader({ lang, searchParams = `` }: Props) {
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-500/10'>
               <div className='space-y-2 py-6'>
-                {navigation.map((item) => (
+                {navigation.map((item, i) => (
                   <Link
-                    key={item.name}
+                    key={i}
                     href={item.href}
                     className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
                     {item.name}
