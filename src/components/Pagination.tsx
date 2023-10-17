8a4372687f3a6cf55ba561d7e72b728acf3143cc
uusa35 <uusa35@gmail.com>
2023-10-17 22:21:42 +0300

    image qty to 100

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,13 +1,15 @@
 "use client";
-
 import { has, isEmpty, isNull, map } from "lodash";
 import Link from "next/link";
+import { MainContext } from "./MainContentLayout";
+import { useContext } from "react";
 
 type Props = {
   links: any;
 };
 
 export default function ({ links }: Props): React.ReactNode {
+  const { previous, next }: any = useContext(MainContext);
   function getClassName(active: boolean) {
     if (active) {
       return "mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-gray-200 focus:border-primary focus:text-primary bg-gray-600 text-white hover:text-gray-900";
@@ -18,7 +20,7 @@ export default function ({ links }: Props): React.ReactNode {
 
   return (
     !isEmpty(links) && (
-      <div className='bg-white my-6 rounded-md flex justify-center items-center'>
+      <div className='bg-white my-6 rounded-md flex justify-center items-center capitalize'>
         <div className='flex flex-wrap  px-2 py-4 '>
           {links &&
             map(links, (link: any, i) =>
@@ -26,14 +28,22 @@ export default function ({ links }: Props): React.ReactNode {
                 <div
                   key={i}
                   className='mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded'>
-                  {link.label}
+                  {link.label.includes("Previous")
+                    ? previous
+                    : link.label.includes("Next")
+                    ? next
+                    : link.label}
                 </div>
               ) : (
                 <Link
                   key={i}
                   className={getClassName(link.active)}
                   href={link.url}>
-                  {link.label}
+                  {link.label.includes("Previous")
+                    ? previous
+                    : link.label.includes("Next")
+                    ? next
+                    : link.label}
                 </Link>
               )
             )}
