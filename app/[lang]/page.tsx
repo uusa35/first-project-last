a9e2624c3937621258e8cc59bd5824fe69a90218
uusa35 <uusa35@gmail.com>
2023-10-18 22:40:30 +0300

    fix errors in usershow

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -536,7 +536,7 @@ export default async function Home({ params: { lang } }: Props) {
             <h2 className='text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
               {trans.gallery}
             </h2>
-            <p className='mt-6 text-lg leading-8 text-gray-600'>
+            <p className='mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600'>
               We’re a dynamic group of individuals who are passionate about what
               we do and dedicated to delivering the best results for our
               clients.
