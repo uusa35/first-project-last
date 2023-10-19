ad96d60eb98c87b684dbfad6a6a214e4e81c112a
uusa35 <uusa35@gmail.com>
2023-10-19 20:57:59 +0300

    fix errors in usershow

--- app/[lang]/contactus/page.tsx
+++ app/[lang]/contactus/page.tsx
@@ -2,6 +2,7 @@ import { MainContextLayout } from "@/components/MainContentLayout";
 import { Locale } from "@/types/index";
 import { getDictionary } from "@/lib/dictionary";
 import { getSetting } from "@/utils/setting";
+import Image from "next/image";
 
 export default async function ({
   params: { lang },
@@ -21,10 +22,12 @@ export default async function ({
       setting={setting}>
       <div className='relative bg-white mx-auto max-w-7xl min-h-screen'>
         <div className='lg:absolute lg:inset-0 lg:ltr:left-1/2 lg:rtl:right-1/2'>
-          <img
+          <Image
+            width={1000}
+            height={1000}
             className='h-64 w-full bg-gray-50 object-cover sm:h-80 lg:absolute lg:h-full'
             src='https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80'
-            alt=''
+            alt={setting.name}
           />
         </div>
         <div className='pb-24 pt-16 sm:pb-32 sm:pt-24 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:pt-32'>
--- app/[lang]/order/[reference_id]/page.tsx
+++ app/[lang]/order/[reference_id]/page.tsx
@@ -8,7 +8,6 @@ import { getCountries } from "@/utils/country";
 import OrderDetails from "@/components/order/OrderDetails";
 import Link from "next/link";
 
-
 export default async function ({
   params: { lang, reference_id },
 }: {
@@ -32,7 +31,7 @@ export default async function ({
           <Image
             width={200}
             height={200}
-            src={order.membership.image}
+            src='https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80'
             alt={order.membership.name}
             className='h-full w-full object-cover object-center'
           />
@@ -68,6 +67,61 @@ export default async function ({
               <dl className='mt-16 text-sm font-medium'>
                 <dt className='text-gray-900'>{trans.order_id}</dt>
                 <dd className='mt-2 text-gray-600'>#{order.id}</dd>
+              </dl>
+
+              {/* company name */}
+              <dl className='mt-16 grid grid-cols-1  gap-x-4 text-sm text-gray-600'>
+                <div className=''>
+                  <dt className='font-medium text-gray-900'>
+                    {trans.beneficiary_name}
+                  </dt>
+                  <dd className='mt-2'>
+                    <address className='not-italic gap-3 flex flex-row justify-start items-center'>
+                      <div>
+                        <Image
+                          alt={order.user.name}
+                          src={order.user.image}
+                          width={60}
+                          height={60}
+                          className='object-contain rounded-lg shadow-sm w-20'
+                        />
+                      </div>
+                      <div className='space-y-1'>
+                        <span className='block'>{order.user.name}</span>
+                        <span className='block'>{order.user.email}</span>
+                        <span className='block'>
+                          {order.user.area} {order.user.country?.name}
+                        </span>
+                      </div>
+                    </address>
+                  </dd>
+                </div>
+                <div>
+                  <dt className='font-medium text-gray-900 hidden'>
+                    Payment Information
+                  </dt>
+                  <dd className='mt-2 space-y-2 sm:flex sm:space-x-4 sm:space-y-0 hidden'>
+                    <div className='flex-none hidden'>
+                      <svg
+                        aria-hidden='true'
+                        width={36}
+                        height={24}
+                        viewBox='0 0 36 24'
+                        className='h-6 w-auto'>
+                        <rect width={36} height={24} rx={4} fill='#224DBA' />
+                        <path
+                          d='M10.925 15.673H8.874l-1.538-6c-.073-.276-.228-.52-.456-.635A6.575 6.575 0 005 8.403v-.231h3.304c.456 0 .798.347.855.75l.798 4.328 2.05-5.078h1.994l-3.076 7.5zm4.216 0h-1.937L14.8 8.172h1.937l-1.595 7.5zm4.101-5.422c.057-.404.399-.635.798-.635a3.54 3.54 0 011.88.346l.342-1.615A4.808 4.808 0 0020.496 8c-1.88 0-3.248 1.039-3.248 2.481 0 1.097.969 1.673 1.653 2.02.74.346 1.025.577.968.923 0 .519-.57.75-1.139.75a4.795 4.795 0 01-1.994-.462l-.342 1.616a5.48 5.48 0 002.108.404c2.108.057 3.418-.981 3.418-2.539 0-1.962-2.678-2.077-2.678-2.942zm9.457 5.422L27.16 8.172h-1.652a.858.858 0 00-.798.577l-2.848 6.924h1.994l.398-1.096h2.45l.228 1.096h1.766zm-2.905-5.482l.57 2.827h-1.596l1.026-2.827z'
+                          fill='#fff'
+                        />
+                      </svg>
+                      <p className='sr-only'>Visa</p>
+                    </div>
+                    <div className='flex-auto hidden'>
+                      <p className='text-gray-900'>Ending with 4242</p>
+                      <p>Expires 12 / 21</p>
+                    </div>
+                  </dd>
+                </div>
               </dl>
 
               <ul
@@ -114,49 +168,6 @@ export default async function ({
               </ul>
 
               <OrderDetails order={order} country={country} lang={lang} />
-
-              <dl className='mt-16 grid grid-cols-2 py-8 gap-x-4 text-sm text-gray-600'>
-                <div className=''>
-                  <dt className='font-medium text-gray-900'>
-                    {trans.beneficiary_name}
-                  </dt>
-                  <dd className='mt-2'>
-                    <address className='not-italic space-y-2'>
-                      <span className='block'>{order.user.name}</span>
-                      <span className='block'></span>
-                      <span className='block'>
-                        {order.user.area} {order.user.country?.name}
-                      </span>
-                    </address>
-                  </dd>
-                </div>
-                <div>
-                  <dt className='font-medium text-gray-900 hidden'>
-                    Payment Information
-                  </dt>
-                  <dd className='mt-2 space-y-2 sm:flex sm:space-x-4 sm:space-y-0 hidden'>
-                    <div className='flex-none hidden'>
-                      <svg
-                        aria-hidden='true'
-                        width={36}
-                        height={24}
-                        viewBox='0 0 36 24'
-                        className='h-6 w-auto'>
-                        <rect width={36} height={24} rx={4} fill='#224DBA' />
-                        <path
-                          d='M10.925 15.673H8.874l-1.538-6c-.073-.276-.228-.52-.456-.635A6.575 6.575 0 005 8.403v-.231h3.304c.456 0 .798.347.855.75l.798 4.328 2.05-5.078h1.994l-3.076 7.5zm4.216 0h-1.937L14.8 8.172h1.937l-1.595 7.5zm4.101-5.422c.057-.404.399-.635.798-.635a3.54 3.54 0 011.88.346l.342-1.615A4.808 4.808 0 0020.496 8c-1.88 0-3.248 1.039-3.248 2.481 0 1.097.969 1.673 1.653 2.02.74.346 1.025.577.968.923 0 .519-.57.75-1.139.75a4.795 4.795 0 01-1.994-.462l-.342 1.616a5.48 5.48 0 002.108.404c2.108.057 3.418-.981 3.418-2.539 0-1.962-2.678-2.077-2.678-2.942zm9.457 5.422L27.16 8.172h-1.652a.858.858 0 00-.798.577l-2.848 6.924h1.994l.398-1.096h2.45l.228 1.096h1.766zm-2.905-5.482l.57 2.827h-1.596l1.026-2.827z'
-                          fill='#fff'
-                        />
-                      </svg>
-                      <p className='sr-only'>Visa</p>
-                    </div>
-                    <div className='flex-auto hidden'>
-                      <p className='text-gray-900'>Ending with 4242</p>
-                      <p>Expires 12 / 21</p>
-                    </div>
-                  </dd>
-                </div>
-              </dl>
 
               <div className='mt-16 border-t border-gray-200 py-6 ltr:text-right rtl:text-left capitalize'>
                 <Link
