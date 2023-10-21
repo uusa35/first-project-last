1295aea17907e41b9accc0d49080a8485306bd8a
uusa35 <uusa35@gmail.com>
2023-10-21 21:10:10 +0300

    from eren

--- src/components/contactus/ContactusContent.tsx
+++ src/components/contactus/ContactusContent.tsx
@@ -13,9 +13,18 @@ import { disableLoading, enableLoading } from "@/redux/slices/settingSlice";
 import { Spinner } from "@material-tailwind/react";
 import LoadingSpinner from "../LoadingSpinner";
 import { apiUrl } from "@/src/constants";
+import { contactusSchema } from "@/src/validations";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type Props = {
   lang: Locale["lang"];
+};
+type Inputs = {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
 };
 
 export default function ({ lang }: Props) {
@@ -31,126 +40,154 @@ export default function ({ lang }: Props) {
   const {
     appSetting: { isLoading },
   } = useAppSelector((state) => state);
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  }: any = useForm<Inputs>({
+    resolver: yupResolver(contactusSchema),
+    defaultValues: {
+      name: ``,
+      email: ``,
+      phone: ``,
+      message: ``,
+    },
+  });
 
   const dispatch = useAppDispatch();
   const [triggerSendContactus, { data, error }] = useLazySendContactusQuery();
 
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const onSubmit: SubmitHandler<Inputs> = (body: any) => {
     dispatch(
       showWarningToastMessage({
         content: ur_request_is_pending_processed_plz_wait,
       })
     );
-    e.preventDefault();
     console.log("the url", apiUrl);
-    const form = e.currentTarget;
-    const formData = new FormData(form);
-    const formDataObject = Object.fromEntries(formData);
     dispatch(enableLoading());
-    return await triggerSendContactus({ lang, body: formDataObject })
+    triggerSendContactus({ lang, body })
       .then((r: any) => {
-        if (r.error && r.error.data) {
+        if (r && r.data && r.data.message) {
+          dispatch(showSuccessToastMessage({ content: r.data.message }));
+        } else if (r && r.error && r.error.data) {
           dispatch(showErrorToastMessage({ content: r.error.data.message }));
-        } else if (r.data && r.data.messsage) {
-          dispatch(showSuccessToastMessage({ content: r.data?.message }));
-          form.reset();
         }
       })
       .then(() => dispatch(disableLoading()));
   };
 
-  if (isLoading) return <LoadingSpinner />;
-
   return (
-    <form onSubmit={(e) => onSubmit(e)} className='mt-16'>
-      <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
-        <div className='sm:col-span-2'>
-          <label
-            htmlFor='name'
-            className='block text-sm font-semibold leading-6 text-gray-900'>
-            {name}*
-          </label>
-
-          <div className='mt-2.5'>
-            <input
-              type='text'
-              name='name'
-              id='name'
-              required
-              autoComplete='given-name'
-              className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-            />
-          </div>
-        </div>
-
-        <div className='sm:col-span-2'>
-          <label
-            htmlFor='email'
-            className='block text-sm font-semibold leading-6 text-gray-900'>
-            {email}*
-          </label>
-
-          <div className='mt-2.5'>
-            <input
-              id='email'
-              name='email'
-              type='email'
-              required
-              autoComplete='email'
-              className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-            />
-          </div>
-        </div>
-
-        <div className='sm:col-span-2'>
-          <div className='flex justify-between text-sm leading-6'>
+    <>
+      {isLoading && <LoadingSpinner />}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={`mt-16 ${isLoading && "hidden"}`}>
+        <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
+          <div className='sm:col-span-2'>
             <label
-              htmlFor='phone'
-              className='block font-semibold text-gray-900'>
-              {phone}*
-            </label>
-          </div>
-          <div className='mt-2.5'>
-            <input
-              type='string'
-              name='phone'
-              id='phone'
-              required
-              autoComplete='tel'
-              aria-describedby='phone-description'
-              className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-            />
-          </div>
-        </div>
-        <div className='sm:col-span-2'>
-          <div className='flex justify-between text-sm leading-6'>
-            <label
-              htmlFor='message'
+              htmlFor='name'
               className='block text-sm font-semibold leading-6 text-gray-900'>
-              {message}*
+              {/* {name}* */}
             </label>
-            <p id='message-description' className='text-gray-400'></p>
+
+            <div className='mt-2.5'>
+              <input
+                type='text'
+                {...register("name")}
+                id='name'
+                autoComplete='given-name'
+                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+            {errors?.name?.message && (
+              <span className={`text-red-700 text-xs capitalize`}>
+                {errors?.name?.message}
+              </span>
+            )}
           </div>
-          <div className='mt-2.5'>
-            <textarea
-              id='message'
-              name='message'
-              rows={4}
-              required
-              aria-describedby='message-description'
-              className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-              defaultValue={""}
-            />
+
+          <div className='sm:col-span-2'>
+            <label
+              htmlFor='email'
+              className='block text-sm font-semibold leading-6 text-gray-900'>
+              {email}*
+            </label>
+
+            <div className='mt-2.5'>
+              <input
+                id='email'
+                {...register("email")}
+                type='email'
+                autoComplete='email'
+                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+            {errors?.email?.message && (
+              <span className={`text-red-700 text-xs capitalize`}>
+                {errors?.email?.message}
+              </span>
+            )}
+          </div>
+
+          <div className='sm:col-span-2'>
+            <div className='flex justify-between text-sm leading-6'>
+              <label
+                htmlFor='phone'
+                className='block font-semibold text-gray-900'>
+                {phone}*
+              </label>
+            </div>
+            <div className='mt-2.5'>
+              <input
+                type='text'
+                {...register("phone")}
+                id='phone'
+                autoComplete='tel'
+                aria-describedby='phone-description'
+                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+            {errors?.phone?.message && (
+              <span className={`text-red-700 text-xs capitalize`}>
+                {errors?.phone?.message}
+              </span>
+            )}
+          </div>
+          <div className='sm:col-span-2'>
+            <div className='flex justify-between text-sm leading-6'>
+              <label
+                htmlFor='message'
+                className='block text-sm font-semibold leading-6 text-gray-900'>
+                {message}*
+              </label>
+              <p id='message-description' className='text-gray-400'></p>
+            </div>
+            <div className='mt-2.5'>
+              <textarea
+                id='message'
+                {...register("message")}
+                rows={4}
+                aria-describedby='message-description'
+                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                defaultValue={""}
+              />
+              {errors?.message?.message && (
+                <span className={`text-red-700 text-xs capitalize`}>
+                  {errors?.message?.message}
+                </span>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      <div className='mt-10 flex justify-end border-t border-gray-900/10 pt-8'>
-        <button
-          type='submit'
-          className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-          {send}
-        </button>
-      </div>
-    </form>
+        <div className='mt-10 flex justify-end border-t border-gray-900/10 pt-8'>
+          <button
+            type='submit'
+            className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+            {send}
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
