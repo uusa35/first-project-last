c7817ffccce8ea88b6f2bf7be5ff9c456a8c90cc
uusa35 <uusa35@gmail.com>
2023-10-23 14:49:26 +0300

    middlware for payment redireciton

--- src/components/order/OrderDetails.tsx
+++ src/components/order/OrderDetails.tsx
@@ -17,19 +17,19 @@ export default function ({
   lang,
   dollarCountry,
 }: Props): React.ReactNode {
-  const { total, net_total, discount }: any = useContext(MainContext);
+  const trans: any = useContext(MainContext);
 
   return (
     <dl className='space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500 capitalize'>
       <div className='flex justify-between'>
-        <dt>{total}</dt>
+        <dt>{trans.total}</dt>
         <dd className='text-gray-900'>
           {getPrice(order.total, country).toFixed(2)} {country.currency_symbol}
         </dd>
       </div>
 
       <div className='flex justify-between'>
-        <dt>{discount}</dt>
+        <dt>{trans.discount}</dt>
         <dd className='text-gray-900'>
           {getPrice(order.discount, country).toFixed(2)}{" "}
           {country.currency_symbol}
@@ -37,7 +37,7 @@ export default function ({
       </div>
 
       <div className='flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900'>
-        <dt className='text-base'>{net_total}</dt>
+        <dt className='text-base'>{trans.net_total}</dt>
         <dd className='text-base'>
           {getPrice(order.net_total, country).toFixed(2)}{" "}
           {country.currency_symbol}
@@ -46,7 +46,7 @@ export default function ({
       {lang !== "en" && (
         <div className='flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900'>
           <dt className='text-base'>
-            {net_total} ({dollarCountry.name})
+            {trans.net_total} ({trans[dollarCountry.country_code]})
           </dt>
           <dd className='text-base'>
             {getPrice(order.net_total, dollarCountry).toFixed(2)}{" "}
