7b61b918be52c1c75c44cf09253f863201440ef2
uusa35 <uusa35@gmail.com>
2023-10-23 17:39:01 +0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -22,45 +22,32 @@ export function middleware(request: NextRequest) {
     locale => !pathName.startsWith(`/${locale}/`) && pathName !== `/${locale}`
   )
 
-  // Redirect if there is no locale
-  console.log('=====pathnameIsMissingLocale=====', pathnameIsMissingLocale)
-  console.log('====req url', request.url);
+
   // `x-forwarded-host` and `host` headers do not match`origin` header from a forwarded Server Action
   const url = request.nextUrl.clone();
-  // console.log('=======url======', url);
   const isProduction = process.env.NODE_ENV === 'production' // redirect only in production
-  if (url.pathname.includes('order') && isProduction) {
-    console.log('=======if======');
+  if (url.pathname.includes('order') && !isProduction) {
     request.headers.append('x-forwarded-host', 'stspayone.com');
     const requestedHost = request.headers.get('x-forwarded-host');
     // https://srstaging.stspayone.com
-    // console.log('====the x-forward host=====', requestedHost);
-    // console.log('====the all headers=====', request.headers);
     if (requestedHost && requestedHost.match(/stspayone.com/)) {
-      // console.log('=====inside here middleware=======');
       const host = `dev.ar-expo.ru`; // set your main domain
       const requestedPort = request.headers.get('x-forwarded-port');
       const requestedProto = request.headers.get('x-forwarded-proto');
       url.host = host;
       url.protocol = requestedProto || url.protocol;
       url.port = requestedPort || url.port;
-      // console.log('the url', url);
+      const finalPath = `https://${host}${request.nextUrl.pathname}`;
       console.log('=====the url host=====', url.host);
       console.log('=====the url hostname=====', url.hostname);
       console.log('=====the url origin=====', url.hostname);
-      const finalPath = `https://${host}${request.nextUrl.pathname}`;
-      console.log('====final Path=====', `https://${host}${request.nextUrl.pathname}`)
-      // return NextResponse.redirect(new URL(url.pathname, `https://${url.hostname}`));
-      // return NextResponse.redirect(
-      //   `http://${host}${request.nextUrl.pathname}`
-      // );
-      console.log('====path name====', pathName);
+      console.log('====final Path=====', `${finalPath}`)
       return NextResponse.rewrite(new URL(`${pathName}`, finalPath));
     } else {
 
     }
   } else {
-
+    // Redirect if there is no locale
     if (pathnameIsMissingLocale) {
       const locale = getLocale(request)
       return NextResponse.redirect(
