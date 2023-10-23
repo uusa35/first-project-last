cf353446fe0f8387854388309c02fb5172d8b057
uusa35 <uusa35@gmail.com>
2023-10-23 16:03:32 +0300

    middlware for payment redireciton

--- middleware.ts
+++ middleware.ts
@@ -36,22 +36,27 @@ export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
   const isProduction = process.env.NODE_ENV === 'production' // redirect only in production
   if (url.pathname.includes('order') && isProduction) {
-    console.log('=====the url=====', url);
+    // console.log('=====the url=====', url);
     request.headers.append('x-forwarded-host', 'stspayone.com');
     const requestedHost = request.headers.get('x-forwarded-host');
     // https://srstaging.stspayone.com
-    console.log('====the x-forward host=====', requestedHost);
-    console.log('====the all headers=====', request.headers);
+    // console.log('====the x-forward host=====', requestedHost);
+    // console.log('====the all headers=====', request.headers);
     if (requestedHost && requestedHost.match(/stspayone.com/)) {
-      console.log('=====inside here middleware=======');
+      // console.log('=====inside here middleware=======');
       const host = `dev.ar-expo.ru`; // set your main domain
       const requestedPort = request.headers.get('x-forwarded-port');
       const requestedProto = request.headers.get('x-forwarded-proto');
       url.host = host;
       url.protocol = requestedProto || url.protocol;
       url.port = requestedPort || url.port;
-      console.log('the url', url);
-      return NextResponse.redirect(new URL(url.pathname, `https://${url.hostname}`));
+      // console.log('the url', url);
+      console.log('====final Path=====', `https://${url.host}${request.nextUrl.pathname}`)
+      // return NextResponse.redirect(new URL(url.pathname, `https://${url.hostname}`));
+      return NextResponse.redirect(
+        `https://${url.host}${request.nextUrl.pathname}`,
+        301
+      );
     }
     return NextResponse.next();
   }
