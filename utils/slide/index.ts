d379d7a212d0e6eca27f392049bf7d0bb9404fd6
uusa35 <uusa35@gmail.com>
2023-10-24 00:07:51 +0300

    middlware for payment redireciton

--- utils/slide/index.ts
+++ utils/slide/index.ts
@@ -3,11 +3,11 @@ import { NextResponse } from 'next/server'
 
 export async function getSlides(search: string, lang: Locale['lang']) {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}slide?${search}`, {
-        next: { revalidate: 180 },
-        // cache: "no-store",
+        next: { revalidate: 60 },
         headers: {
-            'Accept-Language': lang
-        }
+            'Accept-Language': lang,
+        },
+
     });
     if (!res.ok) return undefined
     return res.json()
