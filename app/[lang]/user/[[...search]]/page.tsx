a4fa21d8a72bfce3985ea64b2ebada12e3b14c4c
uusa35 <uusa35@gmail.com>
2023-10-29 16:10:51 +0300

    clean up code

--- app/[lang]/user/[[...search]]/page.tsx
+++ app/[lang]/user/[[...search]]/page.tsx
@@ -53,29 +53,26 @@ export default async function UserIndex({
       />
     );
 
-  // console.log({ searchParams });
-
   return (
     <MainContextLayout
       trans={trans}
       lang={lang}
       searchParams={searchParams}
-      setting={setting}
-    >
-      <main className="relative isolate mx-auto max-w-7xl min-h-screen">
+      setting={setting}>
+      <main className='relative isolate mx-auto max-w-7xl min-h-screen'>
         {/* Image section */}
-        <div className="mt-8 sm:mt-8 xl:mx-auto xl:max-w-7xl">
-          <div className="absolute left-10 top-10"></div>
-          <div className="absolute w-full lg:max-w-4xl flex flex-col lg:flex-row  justify-center lg:justify-start items-center top-0 lg:top-32 bg-stone/60 lg:rtl:right-10 lg:ltr:left-10 p-8 text-white  gap-4  rounded-md">
-            <div className="flex flex-col justify-center lg:justify-start items-center lg:items-start gap-4 text-center rtl:text-right ltr:text-left">
-              <div className="text-2xl lg:text-6xl capitalize ">
+        <div className='mt-8 sm:mt-8 xl:mx-auto xl:max-w-7xl'>
+          <div className='absolute left-10 top-10'></div>
+          <div className='absolute w-full lg:max-w-4xl flex flex-col lg:flex-row  justify-center lg:justify-start items-center top-0 lg:top-32 bg-stone/60 lg:rtl:right-10 lg:ltr:left-10 p-8 text-white  gap-4  rounded-md'>
+            <div className='flex flex-col justify-center lg:justify-start items-center lg:items-start gap-4 text-center rtl:text-right ltr:text-left'>
+              <div className='text-2xl lg:text-6xl capitalize '>
                 {searchParams &&
                 searchParams.membership &&
                 searchParams.membership === "sponsorship"
                   ? trans.sponsorships
                   : trans.subscriptions}
               </div>
-              <div className="text-lg lg:text-xl capitalize">
+              <div className='text-lg lg:text-xl capitalize'>
                 {searchParams &&
                 searchParams.membership &&
                 searchParams.membership === "sponsorship"
@@ -89,15 +86,15 @@ export default async function UserIndex({
             height={500}
             src={UserIndexBanner}
             alt={setting.name}
-            className="aspect-[9/3] w-full object-cover xl:rounded-lg"
+            className='aspect-[9/3] w-full object-cover xl:rounded-lg'
           />
         </div>
 
-        <div className="px-5">
+        <div className='px-5'>
           {/* filters */}
-          <div className="flex flex-col md:flex-row items-center justify-between gap-x-10 text-sm">
+          <div className='flex flex-col md:flex-row items-center justify-between gap-x-10 text-sm'>
             {/* search categories */}
-            <div className="flex gap-x-3 py-5 overflow-auto scrollbar-hide w-full md:w-3/4">
+            <div className='flex gap-x-3 py-5 overflow-auto scrollbar-hide w-full md:w-3/4'>
               <Link
                 href={`/${lang}/user?membership=${
                   (searchParams as { [key: string]: string }).membership
@@ -106,8 +103,7 @@ export default async function UserIndex({
                   !(searchParams as { [key: string]: string })?.category_id
                     ? "text-white  bg-expo-dark "
                     : "text-expo-dark"
-                }`}
-              >
+                }`}>
                 {trans.all_participants}
               </Link>
               {categories?.data?.map((c: Category, i: number) => (
@@ -121,8 +117,7 @@ export default async function UserIndex({
                     c.id.toString()
                       ? "text-white  bg-expo-dark "
                       : "text-expo-dark"
-                  }`}
-                >
+                  }`}>
                   {c.name}
                 </Link>
               ))}
@@ -133,9 +128,8 @@ export default async function UserIndex({
         </div>
 
         <ul
-          role="list"
-          className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 py-12 after:px-8 px-4 xl:px-0"
-        >
+          role='list'
+          className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 py-12 after:px-8 px-4 xl:px-0'>
           {users.data.map((u: User, i: number) => (
             <UserCard element={u} lang={lang} key={u.name} trans={trans} />
           ))}
