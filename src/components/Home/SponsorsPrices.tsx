95f4d7b8f35280132bde19ed7bed1533bdd4f4f4
uusa35 <uusa35@gmail.com>
2023-10-29 16:40:06 +0300

    clean up code

--- src/components/Home/SponsorsPrices.tsx
+++ src/components/Home/SponsorsPrices.tsx
@@ -14,28 +14,29 @@ type Props = {
 
 export function SponsorsPrices({ trans, sponsorships, lang, country }: Props) {
   return (
-    <div className="bg-expo-green py-12 sm:py-12 capitalize">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-4xl text-center">
-          <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+    <div className='bg-expo-green py-12 sm:py-12 capitalize'>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='mx-auto max-w-4xl text-center'>
+          <p className='mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
             {trans.sponsors}
           </p>
         </div>
-        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+        <p className='mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600'>
           {trans.register_now_and_learn_about_sponsorship_packages}
         </p>
 
-        <div className="flex justify-center mt-5">
-          <Link className="btn-dark-hover" href={`/${lang}/register/company`}>
+        <div className='flex justify-center mt-5'>
+          <Link className='btn-dark-hover' href={`/${lang}/register/company`}>
             {trans.register_as_a_sponsor}
           </Link>
         </div>
 
-        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className='isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
           {sponsorships.map((s: Membership, i: number) => (
             <MembershipCard
               element={s}
               key={i}
+              scaleMiddle={i === 1 && s.is_featured}
               lang={lang}
               country={country[0]}
             />
