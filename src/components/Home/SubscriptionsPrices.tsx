b3e027a268e67560f72047c4217319a83e0d8846
uusa35 <uusa35@gmail.com>
2023-10-29 16:35:08 +0300

    clean up code

--- src/components/Home/SubscriptionsPrices.tsx
+++ src/components/Home/SubscriptionsPrices.tsx
@@ -39,7 +39,7 @@ export function SubscriptionsPrices({
           {subscriptions.map((subscription: Membership, i: number) => (
             <MembershipCard
               scaleOnHover={false}
-              scaleMiddle={i === 1}
+              scaleMiddle={i === 1 && subscription.is_featured}
               element={subscription}
               country={country[0]}
               lang={lang}
