b77bc42dc2e74337a09b1d2e98b7ae08cfedb89f
uusa35 <uusa35@gmail.com>
2023-11-03 11:23:29 +0300

    upgrade

--- src/components/Home/SearchBar.tsx
+++ src/components/Home/SearchBar.tsx
@@ -1,15 +1,16 @@
 "use client";
+import { MainContext } from "@/layouts/MainContentLayout";
 import { Locale } from "@/types/index";
 import Link from "next/link";
-import * as React from "react";
+import { useState, useContext } from "react";
 
 type Props = {
-  trans: { [key: string]: string };
   lang: Locale["lang"];
 };
 
-export function SearchBar({ trans, lang }: Props) {
-  const [searchKey, setSearchKey] = React.useState<string>("");
+export function SearchBar({ lang }: Props) {
+  const [searchKey, setSearchKey] = useState<string>("");
+  const trans: { [key: string]: string } = useContext(MainContext);
 
   return (
     <div className='bg-white'>
@@ -21,15 +22,15 @@ export function SearchBar({ trans, lang }: Props) {
           <div className='mx-auto mt-10 flex max-w-2xl gap-x-4'>
             <input
               required
+              minLength={3}
               className='min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 h-14 text-black bg-gray-100 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6 capitalize'
               placeholder={trans.search}
               onChange={(e) => setSearchKey(e.target.value)}
               value={searchKey}
             />
-            {/* it should search in both meberships not just sunscription????? */}
             <Link
               className='btn-default flex items-center capitalize'
-              href={`/${lang}/user?membership=${"subscription"}&search=${searchKey}`}>
+              href={`/${lang}/user?search=${searchKey}`}>
               {trans.search}
             </Link>
           </div>
