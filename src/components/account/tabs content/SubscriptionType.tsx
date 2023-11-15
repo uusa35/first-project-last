31df83bf103705e8d98f2e3ba5f324423fcc9520
eren <erenyatef.g@gmail.com>
2023-11-15 12:06:12 +0200

    sub type

--- src/components/account/tabs content/SubscriptionType.tsx
+++ src/components/account/tabs content/SubscriptionType.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { MainContext } from "@/layouts/MainContentLayout";
+import { useAppSelector } from "@/redux/hooks";
+import { Tab } from "@headlessui/react";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function SubscriptionType({}: Props) {
+  const { hasValidDeal, deals } = useAppSelector((state) => state.auth);
+  const trans: { [key: string]: string } = React.useContext(MainContext);
+
+  console.log({ hasValidDeal, deals });
+
   return (
-    <div>SubscriptionType</div>
-  )
+    <Tab.Panel>
+      <h1 className="text-2xl mb-10 mt-5">
+        {trans.select_the_region_in_the_gallery}
+      </h1>
+      <div className="bg-gray-50 border border-gray-200 rounded-md">
+        <p>{trnas.zone_a_desc}</p>
+        <p>{trnas.zone_b_desc}</p>
+        <p>{trnas.zone_c_desc}</p>
+        <p>{trnas.zone_d_desc}</p>
+      </div>
+    </Tab.Panel>
+  );
 }
