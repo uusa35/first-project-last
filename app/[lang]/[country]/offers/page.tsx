9c013d65e5096cbea6ee8078b6091df36574d298
uusa35 <uusa35@gmail.com>
2023-12-11 12:30:22 +0300

    search params to vendors and offers

--- app/[lang]/[country]/offers/page.tsx
+++ app/[lang]/[country]/offers/page.tsx
@@ -2,17 +2,27 @@ import { Locale, countriesList } from "@/types/index";
 import { getDictionary } from "@/lib/dictionary";
 import { MainContextLayout } from "@/layouts/MainContentLayout";
 import { cookies } from "next/headers";
+import { getProducts } from "@/utils/product";
+import { ElementPagination, Product } from "@/src/types/queries";
+import { convertSearchParamsToString } from "@/utils/helpers";
 
 type Props = {
-  params: { lang: Locale["lang"]; country: countriesList };
+  params: { lang: Locale["lang"]; country: countriesList; search: string };
+  searchParams: { [key: string]: string } | string;
 };
 
-export default async function ({ params: { lang, country } }: Props) {
+export default async function (props: Props) {
   const cookieStore = cookies();
+  const {
+    params: { lang, country },
+    searchParams,
+  } = props;
   const token: any = cookieStore.get("token");
-  const [{ trans }]: [{ trans: any }] = await Promise.all([
-    getDictionary(lang),
-  ]);
+  const [{ trans }, products]: [{ trans: any }, ElementPagination<Product[]>] =
+    await Promise.all([
+      getDictionary(lang),
+      getProducts(convertSearchParamsToString(searchParams ?? undefined)),
+    ]);
 
   return (
     <MainContextLayout trans={trans} lang={lang} country={country}>
--- app/[lang]/[country]/vendors/page.tsx
+++ app/[lang]/[country]/vendors/page.tsx
@@ -8,33 +8,37 @@ import Image from "next/image";
 import Link from "next/link";
 import { appLinks } from "@/src/links";
 import Pagination from "@/src/components/Pagination";
+import { convertSearchParamsToString } from "@/utils/helpers";
 
 type Props = {
   params: { lang: Locale["lang"]; country: countriesList; search: string };
+  searchParams: { [key: string]: string } | string;
 };
 
-export default async function ({ params }: Props) {
+export default async function (props: Props) {
+  const {
+    params: { lang, country },
+    searchParams,
+  } = props;
   const cookieStore = cookies();
   const token: any = cookieStore.get("token");
   const [{ trans }, vendors]: [{ trans: any }, ElementPagination<User[]>] =
-    await Promise.all([getDictionary(params.lang), getVendors(params.search)]);
-
-  console.log("params =====>", params);
+    await Promise.all([
+      getDictionary(lang),
+      getVendors(convertSearchParamsToString(searchParams ?? undefined)),
+    ]);
 
   return (
-    <MainContextLayout
-      trans={trans}
-      lang={params.lang}
-      country={params.country}>
-      <h1 className='text-7xl'>Vendors {params.country}</h1>
+    <MainContextLayout trans={trans} lang={lang} country={country}>
+      <h1 className='text-7xl'>Vendors {country}</h1>
       {/* vendors */}
       <div className='flex w-full flex-col flex-wrap justify-between items-center'>
         <h1>Vendors</h1>
         {vendors.data?.map((s: User, i) => (
           <Link
             href={appLinks.vendor(
-              params.lang,
-              params.country,
+              lang,
+              country,
               s.id.toString(),
               s.store_name ?? s.name
             )}>
